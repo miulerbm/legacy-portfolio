8805f6e5297176cb9b2aba3a2ea50939f7113005
miulerbm <miulerbm00@gmail.com>
2024-05-19 12:53:03 -0500

    legacy porftolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "/miulerbm",
+  title: "portfolio",
   description: "Hello World! Miuler here!",
 };
 
